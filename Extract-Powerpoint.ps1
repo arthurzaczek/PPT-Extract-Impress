f25724dc2c5aa8cfc28950cf5fc9e53e5bbceb5f
Arthur Zaczek <arthur@dasz.at>
2012-10-09 14:54:54 +0200

    Adding parameter and switches - first version

--- Extract-Powerpoint.ps1
+++ Extract-Powerpoint.ps1
@@ -1,5 +1,45 @@
-$file = "P:\Impress-Extract-Powerpoint\Software Engineering 1 - 01 - OOP Basics.pptx"
-$outFile = ".\Software Engineering 1 - 01 - OOP Basics.html"
+<# 
+.SYNOPSIS 
+    extract powerpoint
+.DESCRIPTION 
+    Extracts power point slides to impress js using powershell
+.EXAMPLE
+	.\Extract-Powerpoint.ps1 'MyPresentation.pptx'
+	ls *.pptx | foreach { .\Extract-Powerpoint.ps1 $_ }
+.NOTES 
+    Author     : Arthur Zaczek, arthur@dasz.at
+	License    : GNU General Public License (GPL)
+.LINK 
+    http://dasz.at
+#> 
+
+param(
+	[Parameter(HelpMessage="Path to the Powerpoint to extract")]
+	[string]$file,
+	
+	[Parameter(HelpMessage="Simple mode. Extract only shapes with no formating")]
+	[switch]$Simple,
+
+	[Parameter(HelpMessage="Simple mode. Extract only shapes with no formating")]
+	[switch]$Position,
+	
+	[Parameter(HelpMessage="Open the result when finished")]
+	[switch]$Open
+)
+
+if(!$file) {
+	get-help .\Extract-Powerpoint.ps1 -Full
+	exit 1
+}
+
+if(!$Simple) {
+# full style
+	$Position = $true
+}
+
+# ---------------- Init variables ------------------------------
+$file = resolve-path $file
+$outFile = [System.IO.Path]::GetFileNameWithoutExtension($file) + '.html'
 
 $xPos = 0
 $yPos = 0
@@ -13,44 +53,79 @@ $zGap = -50
 $colCount = 5
 $col = 0
 
+# ---------------- helper functions ------------------------------
+function nameToClass($name) {
+	return $name.Replace(" ", "-")
+}
 
-'<!doctype html>' | out-file $outFile
-'<html>' | out-file $outFile -Append
-'<link href="content/impress.css" rel="stylesheet" />' | out-file $outFile -Append
-'<link href="content/ppt.css" rel="stylesheet" />' | out-file $outFile -Append
-'<body class="impress-not-supported">' | out-file $outFile -Append
-'<div id="impress">' | out-file $outFile -Append
+# ---------------- render functions ------------------------------
+function renderHeader() {
+	'<!doctype html>' | out-file $outFile
+	'<html>' | out-file $outFile -Append
+	'<link href="content/impress.css" rel="stylesheet" />' | out-file $outFile -Append
+	'<link href="content/ppt.css" rel="stylesheet" />' | out-file $outFile -Append
+	'<body class="impress-not-supported">' | out-file $outFile -Append
+	'<div id="impress">' | out-file $outFile -Append
+}
 
+function renderFooter() {
+	'<div id="overview" class="step" data-x="3000" data-y="1500" data-scale="10"/>' | out-file $outFile -Append
+	'</div>' | out-file $outFile -Append
+	'<script src="content/impress.js"></script>' | out-file $outFile -Append
+	'<script>impress().init();</script>' | out-file $outFile -Append
+	'</body>' | out-file $outFile -Append
+	'</html>' | out-file $outFile -Append
+}
+
+function renderTextShape($shape) {
+	$strStyle = ""
+	if($Position) {
+		$strStyle = 'style="position: absolute;top:' + $shape.Top + 'px;left:' + $shape.Left + 'px"'
+	}
+	'    <div ' + $strStyle + '" class="' + (nameToClass $shape.Name) + '">' | out-file $outFile -Append
+	foreach($p in $shape.TextFrame2.TextRange.Paragraphs()) {
+		$fontStyle = 'font-size: ' + $p.Font.Size + 'pt'
+		if($p.Text -and $p.Text.Trim()) {
+			if($p.ParagraphFormat.Bullet.Visible) {
+				$margin = ($p.ParagraphFormat.IndentLevel - 1) * 20
+				('        <li style="margin-left:' + $margin + 'px;' + $fontStyle + '">' + $p.Text + '</li>') | out-file $outFile -Append
+			} else {
+				('        <p style="' + $fontStyle + '">' + $p.Text.TrimEnd() + '</p>') | out-file $outFile -Append
+			} 
+		}
+		else {
+			'        <p style="' + $fontStyle + '">&nbsp;</p>' | out-file $outFile -Append
+		}
+	}
+	'    </div>' | out-file $outFile -Append
+}
+
+function renderSlide($slide) {
+    '<!-- ' + $slide.Name + ' -->' | out-file $outFile -Append
+    '<div class="step slide ' + (nameToClass $slide.CustomLayout.Name) + '" data-x="' + $xPos + '" data-y="' + $yPos + '" data-z="' + $zPos + '">' | out-file $outFile -Append
+    foreach($shape in $slide.Shapes) {
+        if($shape.HasTextFrame) {
+			renderTextShape $shape
+        }
+    }
+    '</div>' | out-file $outFile -Append
+}
+
+# ---------------- Main ------------------------------
+'Extracting "' + $file + '"' | out-host
+'to         "' + $outFile + '"' | out-host
+renderHeader
+
+# init powerpoint
 Add-type -AssemblyName office
 $app = New-Object -ComObject powerpoint.application
-
 $presentation = $app.Presentations.open($file)
 
 foreach($slide in $presentation.Slides) {
     ("-> " + $slide.Name) | out-host
-    '<!-- ' + $slide.Name + ' -->' | out-file $outFile -Append
-    '<div class="step slide ' + $slide.CustomLayout.Name.Replace(" ", "-") + '" data-x="' + $xPos + '" data-y="' + $yPos + '" data-z="' + $zPos + '">' | out-file $outFile -Append
-    foreach($shape in $slide.Shapes) {
-        if($shape.HasTextFrame) {
-            '    <div style="position: absolute;top:' + $shape.Top + 'px;left:' + $shape.Left + 'px" class="' + $shape.Name.Replace(" ", "-") + '">' | out-file $outFile -Append
-            foreach($p in $shape.TextFrame2.TextRange.Paragraphs()) {
-                $fontStyle = 'font-size: ' + $p.Font.Size + 'pt'
-                if($p.Text -and $p.Text.Trim()) {
-                    if($p.ParagraphFormat.Bullet.Visible) {
-                        $margin = ($p.ParagraphFormat.IndentLevel - 1) * 20
-                        ('        <li style="margin-left:' + $margin + 'px;' + $fontStyle + '">' + $p.Text + '</li>') | out-file $outFile -Append
-                    } else {
-                        ('        <p style="' + $fontStyle + '">' + $p.Text.TrimEnd() + '</p>') | out-file $outFile -Append
-                    } 
-                }
-                else {
-                    '        <p style="' + $fontStyle + '">&nbsp;</p>' | out-file $outFile -Append
-                }
-            }
-            '    </div>' | out-file $outFile -Append
-        }
-    }
-    '</div>' | out-file $outFile -Append
+
+	renderSlide $slide
+	
     $xPos += $width + $gap
     $zPos += $zGap
     $col++
@@ -62,13 +137,9 @@ foreach($slide in $presentation.Slides) {
     }
 }
 
-'<div id="overview" class="step" data-x="3000" data-y="1500" data-scale="10"/>' | out-file $outFile -Append
-'</div>' | out-file $outFile -Append
-'<script src="content/impress.js"></script>' | out-file $outFile -Append
-'<script>impress().init();</script>' | out-file $outFile -Append
-'</body>' | out-file $outFile -Append
-'</html>' | out-file $outFile -Append
+renderFooter
 
+# Quit powerpoint
 $app.quit()
 $app = $null
 [gc]::collect()
@@ -76,4 +147,6 @@ $app = $null
 
 "finished...." | out-host
 
-& .\$outFile
+if($Open) {
+	& .\$outFile
+}
