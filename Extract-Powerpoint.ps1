2326961e34c6d4f2e62fc5474c423e15c1cc1a9c
Arthur Zaczek <arthur@dasz.at>
2012-10-09 16:31:26 +0200

    adding syntax highlighter feature

--- Extract-Powerpoint.ps1
+++ Extract-Powerpoint.ps1
@@ -5,6 +5,7 @@
     Extracts power point slides to impress js using powershell
 .EXAMPLE
 	.\Extract-Powerpoint.ps1 'MyPresentation.pptx'
+.EXAMPLE
 	ls *.pptx | % { .\Extract-Powerpoint.ps1 $_ }
 .NOTES 
     Author     : Arthur Zaczek, arthur@dasz.at
@@ -25,6 +26,9 @@ param(
 
 	[Parameter(HelpMessage="Extract font style")]
 	[switch]$FontStyle,
+
+	[Parameter(HelpMessage="Extract each text frame without any bullet as source code")]
+	[switch]$SourceCode,
 	
 	[Parameter(HelpMessage="Open the result when finished")]
 	[switch]$Open
@@ -69,33 +73,62 @@ function getStyleAttribute([string[]]$styles) {
 	return ' style="' + [string]::join(';', $styles) + '"'
 }
 
+function hasBullets($shape) {
+    foreach($p in $shape.TextFrame2.TextRange.Paragraphs()) {
+	   if($p.ParagraphFormat.Bullet.Visible) {
+            return $true; 
+       }
+    }
+    return $false;
+}
+
+function isSingleParagraph($shape) {
+    return $shape.TextFrame2.TextRange.Paragraphs().Count -le 1;
+}
+
+function out-result {
+    $input | out-file $outFile -Append -Encoding "UTF8"
+}
+
 # ---------------- render functions ------------------------------
 function renderHeader() {
-	'<!doctype html>' | out-file $outFile
-	'<html>' | out-file $outFile -Append
-	'<link href="content/impress.css" rel="stylesheet" />' | out-file $outFile -Append
-	'<link href="content/ppt.css" rel="stylesheet" />' | out-file $outFile -Append
-	'<body class="impress-not-supported">' | out-file $outFile -Append
-	'<div id="impress">' | out-file $outFile -Append
+	'<!doctype html>' | out-file $outFile -Encoding "UTF8"
+	'<html>' | out-result
+	'<link href="content/impress.css" rel="stylesheet" />' | out-result
+	'<link href="content/ppt.css" rel="stylesheet" />' | out-result
+    if($SourceCode) {
+        '<link href="content/syntaxhighlighter/styles/shCore.css" rel="stylesheet" type="text/css" />' | out-result
+        '<link href="content/syntaxhighlighter/styles/shThemeDefault.css" rel="stylesheet" type="text/css" />' | out-result
+    }
+	'<body class="impress-not-supported">' | out-result
+	'<div id="impress">' | out-result
 }
 
 function renderFooter() {
-	'<div id="overview" class="step" data-x="3000" data-y="1500" data-scale="10"/>' | out-file $outFile -Append
-	'</div>' | out-file $outFile -Append
-	'<script src="content/impress.js"></script>' | out-file $outFile -Append
-	'<script>impress().init();</script>' | out-file $outFile -Append
-	'</body>' | out-file $outFile -Append
-	'</html>' | out-file $outFile -Append
+    if($SourceCode) {
+        '<script src="content/syntaxhighlighter/scripts/shCore.js" type="text/javascript"></script>' | out-result
+        '<script src="content/syntaxhighlighter/scripts/shAutoloader.js" type="text/javascript"></script>' | out-result
+         
+        '<script type="text/javascript">' | out-result
+        'SyntaxHighlighter.autoloader(' | out-result
+        "  'js jscript javascript  content/syntaxhighlighter/scripts/shBrushJScript.js'," | out-result
+        "  'java                   content/syntaxhighlighter/scripts/shBrushJava.js'," | out-result
+        "  'cpp                    content/syntaxhighlighter/scripts/shBrushCpp.js'," | out-result
+        "  'csharp                 content/syntaxhighlighter/scripts/shBrushCSharp.js'" | out-result
+        ');' | out-result
+         
+        'SyntaxHighlighter.all();' | out-result
+        '</script>' | out-result
+    }
+	'<div id="overview" class="step" data-x="3000" data-y="1500" data-scale="10"/>' | out-result
+	'</div>' | out-result
+	'<script src="content/impress.js"></script>' | out-result
+	'<script>impress().init();</script>' | out-result
+	'</body>' | out-result
+	'</html>' | out-result
 }
 
-function renderTextShape($shape) {
-	$styles = @()
-	if($Position) {
-		$styles += ('position: absolute')
-        $styles += ('top:' + $shape.Top + 'px')
-        $styles += ('left:' + $shape.Left + 'px')
-	}
-	'    <div' + (getStyleAttribute $styles) + ' class="' + (nameToClass $shape.Name) + '">' | out-file $outFile -Append
+function renderParagraphs($shape) {
 	foreach($p in $shape.TextFrame2.TextRange.Paragraphs()) {
 		$styles = @()
 		if($FontStyle) {
@@ -107,27 +140,46 @@ function renderTextShape($shape) {
                 if($margin -gt 0) {
                     $styles += ('margin-left:' + $margin + 'px')
                 }
-				('        <li' + (getStyleAttribute $styles) + '>' + $p.Text + '</li>') | out-file $outFile -Append
+				('        <li' + (getStyleAttribute $styles) + '>' + $p.Text + '</li>') | out-result
 			} else {
-				('        <p' + (getStyleAttribute $styles) + '>' + $p.Text.TrimEnd() + '</p>') | out-file $outFile -Append
+				('        <p' + (getStyleAttribute $styles) + '>' + $p.Text.TrimEnd() + '</p>') | out-result
 			} 
 		}
 		else {
-			'        <p' + (getStyleAttribute $styles) + '>&nbsp;</p>' | out-file $outFile -Append
+			'        <p' + (getStyleAttribute $styles) + '>&nbsp;</p>' | out-result
 		}
 	}
-	'    </div>' | out-file $outFile -Append
+}
+
+function renderSourceCode($shape) {
+    '        <pre class="brush: csharp">' + $shape.TextFrame2.TextRange.Text.Replace("<", "&lt;")  + '</pre>' | out-result
+}
+
+function renderTextShape($shape) {
+	$styles = @()
+	if($Position) {
+		$styles += ('position: absolute')
+        $styles += ('top:' + $shape.Top + 'px')
+        $styles += ('left:' + $shape.Left + 'px')
+	}
+	'    <div' + (getStyleAttribute $styles) + ' class="' + (nameToClass $shape.Name) + '">' | out-result
+    if($SourceCode -and !(hasBullets $shape) -and !(isSingleParagraph $shape)) {
+        renderSourceCode $shape
+    } else {
+        renderParagraphs $shape
+    }
+	'    </div>' | out-result
 }
 
 function renderSlide($slide) {
-    '<!-- ' + $slide.Name + ' -->' | out-file $outFile -Append
-    '<div class="step slide ' + (nameToClass $slide.CustomLayout.Name) + '" data-x="' + $xPos + '" data-y="' + $yPos + '" data-z="' + $zPos + '">' | out-file $outFile -Append
+    '<!-- ' + $slide.Name + ' -->' | out-result
+    '<div class="step slide ' + (nameToClass $slide.CustomLayout.Name) + '" data-x="' + $xPos + '" data-y="' + $yPos + '" data-z="' + $zPos + '">' | out-result
     foreach($shape in $slide.Shapes) {
         if($shape.HasTextFrame) {
 			renderTextShape $shape
         }
     }
-    '</div>' | out-file $outFile -Append
+    '</div>' | out-result
 }
 
 # ---------------- Main ------------------------------
